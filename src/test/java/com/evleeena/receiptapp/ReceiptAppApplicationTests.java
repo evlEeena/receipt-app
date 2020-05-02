56070f4e7ba601838fcf2e541deaaf89ddc6702a
Zaets <zaets@bpcbt.com>
2020-05-02 19:01:27 +0300

    Initial commit

---
+++ src/test/java/com/evleeena/receiptapp/ReceiptAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evleeena.receiptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
