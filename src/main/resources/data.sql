2c8a07f210c2435669ef43c070a2cbe098b25051
evleeena <evleeena@gmail.com>
2020-07-02 21:14:16 +0300

    added initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into category(description) values ('American');
+insert into category(description) values ('Italian');
+insert into category(description) values ('Russian');
+insert into category(description) values ('Fast food');
+
+insert into unit_of_measure(uom) values ('Teaspoon');
+insert into unit_of_measure(uom) values ('Tablespoon');
+insert into unit_of_measure(uom) values ('Cup');
+insert into unit_of_measure(uom) values ('Punch');
