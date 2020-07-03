14dff5bfb02961c2126de2f142543d18c9e6181b
evleeena <evleeena@gmail.com>
2020-07-03 14:06:33 +0300

    added find methods

--- src/main/java/com/evleeena/receiptapp/controllers/IndexController.java
+++ src/main/java/com/evleeena/receiptapp/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.evleeena.receiptapp.controllers;
 
+import com.evleeena.receiptapp.domain.Category;
+import com.evleeena.receiptapp.domain.UnitOfMeasure;
+import com.evleeena.receiptapp.repositories.CategoryRepository;
+import com.evleeena.receiptapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","/index.html"})
+    @Resource
+    private CategoryRepository categoryRepository;
+
+    @Resource
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @RequestMapping({"", "/", "/index.html"})
     public String getIndexPage() {
+
+        Optional<Category> category = categoryRepository.findByDescription("Russian");
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByUom("Cup");
+
+        System.out.println("Category: " + category);
+        System.out.println("UOM: " + uom);
+
         return "index";
     }
 }
--- src/main/java/com/evleeena/receiptapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/evleeena/receiptapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.evleeena.receiptapp.repositories;
 import com.evleeena.receiptapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByUom(String oum);
 }
