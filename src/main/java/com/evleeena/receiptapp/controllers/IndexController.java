c40ef23e803a9e6b399cda0d6e69d95df890de31
evleeena <evleeena@gmail.com>
2020-07-08 16:27:38 +0300

    show recipes

--- src/main/java/com/evleeena/receiptapp/controllers/IndexController.java
+++ src/main/java/com/evleeena/receiptapp/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package com.evleeena.receiptapp.controllers;
 
 import com.evleeena.receiptapp.domain.Category;
+import com.evleeena.receiptapp.domain.Recipe;
 import com.evleeena.receiptapp.domain.UnitOfMeasure;
 import com.evleeena.receiptapp.repositories.CategoryRepository;
+import com.evleeena.receiptapp.repositories.RecipeRepository;
 import com.evleeena.receiptapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -19,14 +22,20 @@ public class IndexController {
     @Resource
     private UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Resource
+    private RecipeRepository recipeRepository;
+
     @RequestMapping({"", "/", "/index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
 
         Optional<Category> category = categoryRepository.findByDescription("Russian");
         Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByUom("Cup");
 
         System.out.println("Category: " + category);
         System.out.println("UOM: " + uom);
+
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        model.addAttribute("recipes", recipes);
 
         return "index";
     }
