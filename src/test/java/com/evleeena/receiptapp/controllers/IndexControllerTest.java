827c447fea778f358be7420da6b7f00a40f5c4d0
evleeena <evleeena@gmail.com>
2020-07-25 21:18:10 +0300

    added mock mvc test

--- src/test/java/com/evleeena/receiptapp/controllers/IndexControllerTest.java
+++ src/test/java/com/evleeena/receiptapp/controllers/IndexControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -17,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -36,7 +41,15 @@ class IndexControllerTest {
     }
 
     @Test
-    void getIndexPage() {
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexPage() {
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(Recipe.builder().description("Chocolate Cake").build());
         recipes.add(Recipe.builder().description("Ice cream").build());
